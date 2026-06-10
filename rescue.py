c00f814f86ef6472c044adacc4c1fb5e00a68de9
rittam0 <rittamkhandai@gmail.com>
2026-06-10 19:40:25 +0530

    feat: implement list iteration and pallindrome

--- rescue.py
+++ rescue.py
@@ -1,8 +1,9 @@
-word="Racecar"
-clean= word.lower()
-print(clean)
-
-reverse_word= clean[::-1]
-print(reverse_word)
-is_match= (clean==reverse_word)
-print("is it a pallindrome", is_match)
+word_list= ["Racecar", "Vellore", "Madam"]
+valid_palindromes=[]
+for current_word in word_list:
+    clean= current_word.lower()
+    reverse_word= clean[::-1]
+    is_match= (clean==reverse_word)
+    if is_match:
+        valid_palindromes.append(current_word)
+print(f"Valid palindromes found: {valid_palindromes}")
