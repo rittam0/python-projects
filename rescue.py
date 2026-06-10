b3a9150b933db25df0d703d4a1af0b306675d309
rittam0 <rittamkhandai@gmail.com>
2026-06-10 21:02:49 +0530

    feat: leetcode 217

--- rescue.py
+++ rescue.py
@@ -1,7 +1,8 @@
-nums= [2,7,11,15]
-target=9
-for  i in range(len(nums)):
-    for j in range(i+1,len(nums)):
-        if nums[i]+nums[j]==target:
-            print(f"match found at index {i} and index {j}")
-            print(f"the numbers are {nums[i]}+{nums[j]}={target}")
+nums= [10,20,30,40]
+sorted_nums= sorted(nums)
+has_duplicate=False
+for i in range(len(sorted_nums)):
+    if sorted_nums[i]==sorted_nums[i+1]:
+        has_duplicate=True
+        break
+print(f"does array contain duplicate: {has_duplicate}")
