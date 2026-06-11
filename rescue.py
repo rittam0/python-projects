f91ca378a75ede1b91180413b491a9f30b235f55
rittam0 <rittamkhandai@gmail.com>
2026-06-11 17:40:41 +0530

    feat: append insert del

--- rescue.py
+++ rescue.py
@@ -1,8 +1,5 @@
-nums= [10,20,30,40]
-sorted_nums= sorted(nums)
-has_duplicate=False
-for i in range(len(sorted_nums)):
-    if sorted_nums[i]==sorted_nums[i+1]:
-        has_duplicate=True
-        break
-print(f"does array contain duplicate: {has_duplicate}")
+fruits= ["apple", "banana", "orange"]
+fruits.append("mango")
+fruits.insert(0, "grape")
+del fruits[2]
+print(fruits)
